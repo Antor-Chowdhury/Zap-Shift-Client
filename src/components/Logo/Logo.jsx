6011229603b75a4c4e63e03bb2141a58be4ae106
Antor-Chowdhury <antor1347@gmail.com>
2026-01-03 00:53:32 +0600

    added logo in navbar

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+
+const Logo = () => {
+  return (
+    <div className="flex items-end mb-4">
+      <img src={logo} alt="" />
+      <h3 className="font-extrabold text-3xl -ms-2.5 -mb-1">ZapShift</h3>
+    </div>
+  );
+};
+
+export default Logo;

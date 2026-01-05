f6ce7dfea9fc9fa194a4d66bb55cf915e68b8406
Antor-Chowdhury <antor1347@gmail.com>
2026-01-05 22:40:29 +0600

    added coverage page

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Coverage = () => {
-  return <div></div>;
+  return (
+    <div className="max-w-[90%] mx-auto bg-white px-27 py-20 rounded-3xl mb-31">
+      <div>
+        <h2 className="font-extrabold text-[56px] text-secondary">
+          We are available in 64 districts
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Coverage;
